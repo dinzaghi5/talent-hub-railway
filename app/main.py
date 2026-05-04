97f494c7b21a53122c5b7e56c98b7e18d92467d2
dinzaghi5 <diasbayuinzaghi.dev@gmail.com>
2026-05-04 21:22:23 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     docs_url=f"{settings.API_V1_STR}/docs",
-    redirect_slashes=False,
 )
 
 # Set all CORS enabled origins
